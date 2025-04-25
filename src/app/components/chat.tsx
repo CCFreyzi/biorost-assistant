a36d3a7cd2c1db13e565936c9b53bc2d29d1663c
CCFreyzi <maksmatsiaka@gmail.com>
2025-04-25 16:47:20 +0300

    add restart chat

--- src/app/components/chat.tsx
+++ src/app/components/chat.tsx
@@ -3,16 +3,27 @@
 import { Message, useAssistant } from "@ai-sdk/react";
 import { useEffect, useRef, useState } from "react";
 import { FiMessageSquare, FiX, FiSend, FiTrash2 } from "react-icons/fi";
+import { VscDebugRestart } from "react-icons/vsc";
 
 export default function Chat() {
+  // const [threadId, setThreadId] = useState<string | undefined>(undefined);
+
   const [isOpen, setIsOpen] = useState(false);
   const messagesEndRef = useRef<HTMLDivElement>(null);
   const [faqClicked, setFaqClicked] = useState(false);
+  const [hovered, setHovered] = useState(false);
 
-  const { status, messages, input, submitMessage, handleInputChange } =
-    useAssistant({
-      api: "/api/assistant",
-    });
+  const {
+    status,
+    messages,
+    input,
+    submitMessage,
+    handleInputChange,
+    setMessages,
+    setThreadId,
+  } = useAssistant({
+    api: "/api/assistant",
+  });
 
   const formRef = useRef<HTMLFormElement>(null);
 
@@ -23,10 +34,10 @@ export default function Chat() {
   const toggleChat = () => setIsOpen(!isOpen);
 
   const faqs = [
-    "Які типи добрив у вас є?",
-    "Які продукти використовуються при листовій обробці навесні?",
-    "Які препарати застосовуються у фазу цвітіння ріпаку?",
-    "Як підготувати посіви до морозів?",
+    "Чи є продукція «Біорост» екологічно безпечною?",
+    "Які основні види добрив і препаратів пропонує компанія «Біорост»?",
+    "Які переваги використання добрив «Біорост» для ґрунту, рослин і людини?",
+    "Які контакти компанії «Біорост» для консультацій і замовлення продукції?",
   ];
 
   const cleanMessageText = (text: string) => {
@@ -43,14 +54,22 @@ export default function Chat() {
 
   useEffect(() => {
     if (faqClicked && input && formRef.current) {
-      const formEvent = new Event('submit', { bubbles: true }) as unknown as React.FormEvent<HTMLFormElement>;
-      Object.defineProperty(formEvent, 'preventDefault', { value: () => {} });
-      Object.defineProperty(formEvent, 'currentTarget', { value: formRef.current });
-      
+      const formEvent = new Event("submit", {
+        bubbles: true,
+      }) as unknown as React.FormEvent<HTMLFormElement>;
+      Object.defineProperty(formEvent, "preventDefault", { value: () => {} });
+      Object.defineProperty(formEvent, "currentTarget", {
+        value: formRef.current,
+      });
+
       submitMessage(formEvent);
       setFaqClicked(false);
     }
   }, [input, faqClicked, submitMessage]);
+
+  function onRestart() {
+    setThreadId(undefined);
+  }
 
   return (
     <div className="fixed bottom-0 left-0 right-0 px-2 pb-2 md:bottom-6 md:right-6 md:left-auto md:px-0 z-50">
@@ -62,19 +81,38 @@ export default function Chat() {
           <FiMessageSquare className="text-white text-2xl" />
         </button>
       )}
-  
+
       {isOpen && (
         <div className="w-full mx-auto max-w-[calc(100%-16px)] md:max-w-none md:w-110 h-[550px] bg-white shadow-xl flex flex-col overflow-hidden rounded-md border border-green-100">
           <div className="bg-green-700 text-white p-3 flex justify-between items-center">
             <h2 className="font-semibold">Біорост асистент</h2>
-            <button
-              onClick={toggleChat}
-              className="text-white hover:text-yellow-300 transition-colors"
-            >
-              <FiX className="text-lg" />
-            </button>
+
+            <div className="flex gap-x-4">
+              <div
+                className="relative inline-block"
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+              >
+                <VscDebugRestart
+                  onClick={onRestart}
+                  className="text-xl cursor-pointer"
+                />
+                {hovered && (
+                  <div className="absolute top-full left-1/2 -translate-x-1/2 mt-1 px-2 py-1 bg-black text-white text-sm rounded whitespace-nowrap z-10 shadow">
+                    Оновити
+                  </div>
+                )}
+              </div>
+
+              <button
+                onClick={toggleChat}
+                className="text-white  transition-colors"
+              >
+                <FiX className="text-xl cursor-pointer" />
+              </button>
+            </div>
           </div>
-  
+
           <div className="flex-1 p-4 overflow-y-auto bg-[#E7F5E9]">
             {messages.length === 0 ? (
               <div className="space-y-2">
@@ -99,7 +137,7 @@ export default function Chat() {
                     className={`mb-3 p-3 rounded-lg ${
                       m.role === "user"
                         ? "bg-green-600 text-white ml-auto"
-                        : "bg-white text-green-900 mr-auto border border-green-300"
+                        : "bg-white text-green-900 mr-auto border ml-[30px] border-green-300"
                     }`}
                   >
                     <strong className="block text-sm mb-1">
@@ -117,7 +155,7 @@ export default function Chat() {
             )}
             <div ref={messagesEndRef} />
           </div>
-  
+
           <form
             ref={formRef}
             onSubmit={submitMessage}
@@ -133,9 +171,9 @@ export default function Chat() {
               className="p-2 text-green-800 hover:text-green-600 disabled:opacity-50 transition-colors"
               disabled={!input}
             >
-              <FiTrash2 className="text-lg" />
+              <FiTrash2 className="text-lg cursor-pointer" />
             </button>
-  
+
             <input
               disabled={status !== "awaiting_message"}
               value={input}
@@ -143,18 +181,17 @@ export default function Chat() {
               placeholder="Введіть запитання..."
               className="flex-1 p-2 border border-green-300 rounded focus:outline-none focus:ring-2 focus:ring-green-500 disabled:bg-[#e2fae2] text-green-900"
             />
-  
+
             <button
               type="submit"
               disabled={status !== "awaiting_message" || !input.trim()}
               className="p-2 text-green-800 hover:text-green-600 disabled:opacity-50 transition-colors"
             >
-              <FiSend className="text-lg" />
+              <FiSend className="text-lg cursor-pointer" />
             </button>
           </form>
         </div>
       )}
     </div>
   );
-  
 }
